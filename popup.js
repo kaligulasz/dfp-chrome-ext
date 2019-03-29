cc63950ec35ccba946a50147710888edd156b31b
Darek Zieliński <dariusz.zielinski@performgroup.com>
2019-03-29 10:55:52 +0100

    specify promise return

--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@ chrome.tabs.executeScript(null, {
  * Send a request for our content script
  * @param {string} type "set"|"remove"|"get"
  * @param {*} data just JSON in case of "setting"
+ * @returns {Promise} chain it with .then(response) to receive data in case of "get"
  */
 function sendMessage(type, data) {
   return new Promise((resolve, reject) => {
